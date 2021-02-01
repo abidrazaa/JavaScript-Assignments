8309e8a2f1bc3f6d75010937e51472b73fe2b2ab
Abid Raza <abid.neduet@gmail.com>
2021-02-02 03:51:42 +0500

    assignment week 5

--- assignment5.js
+++ assignment5.js
@@ -18,3 +18,45 @@ function questionTwo(){
 
     }
 }
+
+function questionThree(){
+    var students = ['hassan', 'salman', 'kamran', 'rashid', 'faizan', "noor"];
+    function required(name){
+        if(name.includes("an")){
+            return document.write(name + ", ");
+        }
+    }
+    students.forEach(required);
+}
+
+
+function questionFour(){
+    var sentence = "The quick brown fox jumps over the lazy dog. The";
+    var sentenceArray = sentence.split(" ");
+    var count = 0;
+    function countThe(word){
+        if(word.toLowerCase() == "the"){
+            count+=1
+        }
+    }
+    sentenceArray.forEach(countThe);
+    document.write("The number of 'the' in the sentence : "+count)
+}
+
+function questionFive(){
+    var rows = prompt("Enter the number of rows : ")
+    var matrix=[]
+    for(i=0;i<rows;i++){
+        var row = [];
+        for(j=0;j<rows;j++){
+            if(i==j){
+                row.push("1")
+            }
+            else{
+                row.push("0")
+            }
+        }
+        matrix.push(row);
+    }
+    console.log(matrix);
+}
